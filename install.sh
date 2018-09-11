f820fb87b54a9ccc055cdcdfd6255059948b0ad2
marcaube <marcaube@users.noreply.github.com>
2018-09-11 14:20:32 -0400

    Add my ZSH configuration

--- install.sh
+++ install.sh
@@ -34,8 +34,8 @@ npm install --global yarn
 mkdir $HOME/Code
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-# rm -rf $HOME/.zshrc
-# ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
