87dbebe9eaa136f67e6a92d3ff93adbb56d6ed33
marcaube <marcaube@users.noreply.github.com>
2018-09-12 13:44:30 -0400

    Fix the global gitignore config (again)

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set my global gitignore
-git config global core.excludesfile $HOME/.dotfiles/.gitignore_global
+git global config core.excludesfile $HOME/.dotfiles/.gitignore_global
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
