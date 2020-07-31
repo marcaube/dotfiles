e0065972a273b6c918a07965c906856cc7c7794e
marcaube <marcaube@users.noreply.github.com>
2020-07-31 13:21:12 -0400

    Automatically setup my global git configs

--- install.sh
+++ install.sh
@@ -41,8 +41,9 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-# Set my global gitignore
-git config --global core.excludesfile $HOME/.dotfiles/.gitignore_global
+# Set my global git configs
+rm $HOME/.gitconfig
+cp $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
