9ac0921204812e205ab1bc6ff658eeb08a773c00
marcaube <marcaube@users.noreply.github.com>
2021-03-31 10:18:27 -0400

    Prettify the install script output

--- install.sh
+++ install.sh
@@ -1,30 +1,37 @@
-#!/bin/sh
-
-echo "Setting up your Mac..."
+#! /usr/bin/env bash
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+ENDCOLOR="\e[0m"
+printf "${GREEN}[+] Setting up your Mac...${ENDCOLOR}\n"
 
 # Hide "last login" line when starting a new terminal session
 touch $HOME/.hushlogin
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
+    printf "${GREEN}[+] Installing Homebrew...${ENDCOLOR}\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Update Homebrew recipes
-brew update
+printf "${GREEN}[+] Updating Homebrew packages...${ENDCOLOR}\n"
+brew update > /dev/null
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
+brew tap homebrew/bundle > /dev/null
+brew bundle > /dev/null
 
 # Install Oh My ZSH
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    printf "${GREEN}[+] Installing Oh My ZSH...${ENDCOLOR}\n"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     compaudit | xargs chmod g-w,o-w
 fi
 
 # Add Zsh to the list of shells
 if ! grep -q $(which zsh) /etc/shells; then
+    printf "${GREEN}[+] Setting zsh as the default shell...${ENDCOLOR}\n"
     sudo sh -c 'which zsh >> /etc/shells'
 
     # Make ZSH the default shell environment
@@ -37,10 +44,12 @@ if [ ! -d "$HOME/Code" ]; then
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+printf "${GREEN}[+] Installing dotfiles...${ENDCOLOR}\n"
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink my .vimrc and install plugins
+printf "${GREEN}[+] Configuring vim...${ENDCOLOR}\n"
 if [ -d $HOME/.vimrc ]; then
     mv $HOME/.vimrc $HOME/.vimrc.bak
     ls -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
@@ -60,14 +69,16 @@ if [ ! -d "$HOME/.mackup" ]; then
 fi
 
 # Set my global git configs
+printf "${GREEN}[+] Configuring git...${ENDCOLOR}\n"
 rm $HOME/.gitconfig
 cp $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
-echo "Don't forget to setup your git signing key."
+printf "${YELLOW}[!] Don't forget to setup your git signing key!${ENDCOLOR}\n"
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
 while true; do
-    read -p "Do you wish to overwrite the macOS preferences? [y/n]" yn
+    printf "${BLUE}[?] Do you wish to overwrite the macOS preferences? [y/n]${ENDCOLOR}\n"
+    read yn
     case $yn in
         [Yy]* ) source .macos; break;;
         [Nn]* ) exit;;
