6401cd21e2d5eaa43aac7bed0a0db771ead38e7a
marcaube <marcaube@users.noreply.github.com>
2022-12-19 21:45:09 -0500

    Add Python and Typescript LSP servers

--- vim/init.lua
+++ vim/init.lua
@@ -74,7 +74,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'rust_analyzer', 'sumneko_lua' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
