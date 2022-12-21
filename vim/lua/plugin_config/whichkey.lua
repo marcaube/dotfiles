8c0a0dc567fd466e1589aac032991953e5adfcaf
marcaube <marcaube@users.noreply.github.com>
2022-12-21 08:53:56 -0500

    Improve the look of the whickey menu

--- vim/lua/plugin_config/whichkey.lua
+++ vim/lua/plugin_config/whichkey.lua
@@ -3,6 +3,19 @@ vim.opt.timeoutlen = 250
 
 require('which-key').setup {
   -- ignore_missing = true,
+  window = {
+    border = 'single', -- none, single, double, shadow
+    position = 'bottom', -- bottom, top
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0,
+  },
+  layout = {
+    height = { min = 4, max = 25 }, -- min and max height of the columns
+    width = { min = 20, max = 50 }, -- min and max width of the columns
+    spacing = 10, -- spacing between columns
+    align = 'center', -- align columns left, center or right
+  },
 }
 
 local wk = require('which-key')
