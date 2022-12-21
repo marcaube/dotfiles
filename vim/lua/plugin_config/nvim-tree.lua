adb20c74590c85dbab127170f50ae912e8c892cc
marcaube <marcaube@users.noreply.github.com>
2022-12-21 11:58:12 -0500

    Leave some notes about NvimTree shortcuts

--- vim/lua/plugin_config/nvim-tree.lua
+++ vim/lua/plugin_config/nvim-tree.lua
@@ -8,3 +8,10 @@ require('nvim-tree').setup()
 
 vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<cr>', { desc = 'Find File in Explorer' })
 
+-- `I` to show hidden/ignored folders
+-- `H` to show dotfiles
+-- `R` to refresh the tree
+-- `r` to rename a file
+-- `d` to delete a file
+-- `a` to add a file/directory
+-- `-` to go _up_ a directory
