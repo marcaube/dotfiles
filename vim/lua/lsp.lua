e1e92c9cf7279842a3c91fd2dcce16b96e5b48ae
marcaube <marcaube@users.noreply.github.com>
2023-03-01 16:48:44 -0500

    Use pylsp and ruff instead of pyright

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -45,7 +45,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'rust_analyzer' }
+local servers = { 'rust_analyzer', 'pylsp', 'ruff_lsp', 'lua_ls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
@@ -73,7 +73,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-require('lspconfig').sumneko_lua.setup {
+require('lspconfig').lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
@@ -95,4 +95,52 @@ require('lspconfig').sumneko_lua.setup {
       telemetry = { enable = false },
     },
   },
+}
+
+
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+require('lspconfig').ruff_lsp.setup {
+  on_attach = on_attach,
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+
+-- Configure `python-lsp-server`
+-- Make sure pylsp is installd: `pip install python-lsp-server`
+-- Also make sure that Rope is installed: `pip install pylsp-rope`
+-- See: https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+require('lspconfig').pylsp.setup {
+  cmd = { "pylsp" },
+  filetypes = { "python" },
+  settings = {
+    pylsp = {
+      plugins = {
+        jedi_completion = { enabled = true },
+        jedi_hover = { enabled = true },
+        jedi_references = { enabled = true },
+        jedi_signature_help = { enabled = true },
+        jedi_symbols = { enabled = true, all_scopes = true },
+        pycodestyle = { enabled = false },
+        flake8 = {
+          enabled = true,
+          ignore = {},
+          maxLineLength = 160
+        },
+        pylint = { enabled = false },
+        pydocstyle = { enabled = false },
+        pyflakes = { enabled = false },
+        mccabe = { enabled = false },
+        preload = { enabled = false },
+        rope_completion = { enabled = true },
+        yapf = { enabled = false },
+      },
+    },
+  },
+  on_attach = on_attach
 }
