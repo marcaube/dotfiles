9d50f8a3b9807e159aead96ac64b775a113bde6c
marcaube <marcaube@users.noreply.github.com>
2024-02-29 10:59:56 -0500

    Update the indent-blankline config

--- vim/lua/plugin_config/indent_blankline.lua
+++ vim/lua/plugin_config/indent_blankline.lua
@@ -1,12 +1,4 @@
--- vim.opt.list = true
--- vim.opt.listchars:append "eol:↴"
-
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 -- https://github.com/lukas-reineke/indent-blankline.nvim
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-  -- show_end_of_line = true,
-}
-
+require('ibl').setup()
