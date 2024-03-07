a4c7092ae5e1ee3d54247233e5183caae52bf3ff
marcaube <marcaube@users.noreply.github.com>
2024-03-07 15:32:18 -0500

    Extract key bindings to seperate file and fix up/down history search

---
+++ zsh/key-bindings.zsh
@@ -0,0 +1,17 @@
+# Use emacs key bindings
+bindkey -e
+
+# kitty: jumping words using option + arrows
+# https://github.com/kovidgoyal/kitty/issues/838#issuecomment-770328902
+bindkey "\e[1;3D" backward-word # ⌥←
+bindkey "\e[1;3C" forward-word  # ⌥→
+
+# [Option-Delete] - delete whole forward-word
+# see: https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
+bindkey "\e[3;3~" kill-word # ⌥⌦
+bindkey "\e[3~" delete-char # ⌦
+
+# Start typing + [Up-Arrow] - fuzzy find history forward
+bindkey "^[[A" history-beginning-search-backward
+bindkey "^[[B" history-beginning-search-forward
+
