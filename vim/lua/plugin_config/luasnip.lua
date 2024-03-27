5b3de5d4a60f2c1837d2ec7fe10c5124d5cacccf
marcaube <marcaube@users.noreply.github.com>
2024-03-27 16:41:26 -0400

    Remove extraneous space

--- vim/lua/plugin_config/luasnip.lua
+++ vim/lua/plugin_config/luasnip.lua
@@ -12,7 +12,7 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs( -4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
